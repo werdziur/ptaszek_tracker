3104d98a581d5ebd33b85a9154f20cc909699567
Weronika Dziua <wer.dziura@gmail.com>
2023-11-29 20:56:50 +0100

    feat: add user item function

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import Users from './users/pages/Users';
-import Birds from './birds/pages/Birds';
-import Map from './map/pages/Map';
+// import Birds from './birds/pages/Birds';
+// import Map from './map/pages/Map';
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path='/' element={<Map />} />
+				{/* <Route path='/' element={<Map />} /> */}
 				<Route path='/users' element={<Users />} />
-				<Route path='/birds' element={<Birds />} />
+				{/* <Route path='/birds' element={<Birds />} /> */}
 				<Route path='*' element={<Navigate to='/' replace />} />
 			</Routes>
 		</Router>
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-	return <h1>Users Works!</h1>;
+	//dummy content, later will will fetch from express
+
+	const USERS = [
+		{
+			id: 'w1',
+			name: 'Weronika',
+			image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+			birds: 14,
+		},
+	];
+	return <UsersList items={USERS} />;
 };
 
 export default Users;
