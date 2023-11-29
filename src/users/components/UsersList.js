7257b4f680a600a671977d17d0643377088e87eb
Weronika Dziua <wer.dziura@gmail.com>
2023-11-29 20:25:24 +0100

    feat: add user page function to pass the data to the user list function

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 import './UsersList.css';
+import UserItem from './UserItem';
 
-const UsersList = props => {};
+const UsersList = props => {
+	if (props.items.length === 0) {
+		return (
+			<div>
+				<h2>No users found</h2>
+			</div>
+		);
+	}
+
+	return (
+		<ul>
+			{props.items.map(user => (
+				<UserItem key={user.id} id={user.id} image={user.image} name={user.name} birdsCount={user.birds} />
+			))}
+		</ul>
+	);
+};
 
 export default UsersList;
