205ebc9b9cac555865505341ec7887a7ab11dfb8
Weronika Dziua <wer.dziura@gmail.com>
2023-11-29 23:47:56 +0100

    feat: add css to userItem file

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,26 +1,29 @@
 import React from 'react';
+
+import './UserItem.css';
 import UsersList from './UsersList';
 
-import './UsersList.css';
-
-const UsersItem = props => {
+const UserItem = props => {
 	return (
-		<li>
-			<div>
-				<div>
-					<img src={props.image} alt={props.name} />
+		<li className='card'>
+			<div className='user-item'>
+				<div className='image'>
+					<img className='user-item__image' src={props.image} alt={props.name} />
 				</div>
-				<div>
-					<h2>{props.name}</h2>
-				</div>
-				<div>
-					<h2>
-						{props.birdsCount} {props.birdsCount === 1 ? 'Bird' : 'Birds'}
-					</h2>
+				<div className='user-item__textbox'>
+					<div className='user-item__textbox--main name'>
+						<h2 className='user-item__text name'>{props.name}</h2>
+					</div>
+
+					<div className='user-item__textbox--main'>
+						<h2 className='user-item__text birds'>
+							{props.birdsCount} {props.birdsCount === 1 ? 'Bird' : 'Birds'}
+						</h2>
+					</div>
 				</div>
 			</div>
 		</li>
 	);
 };
 
-export default UsersItem;
+export default UserItem;
