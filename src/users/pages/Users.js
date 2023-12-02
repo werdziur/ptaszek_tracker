95b14fcfa7d2baecd5e0fdd992c197a85296baa5
Weronika Dziua <wer.dziura@gmail.com>
2023-12-02 20:36:44 +0100

    ref: code refactoring to adjust Card element in user item and user list

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -6,42 +6,42 @@ const Users = () => {
 	//dummy content, later will will fetch from express
 
 	const USERS = [
-		{
-			id: 'w1',
-			name: 'Weronika',
-			image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
-			birds: 14,
-		},
-		{
-			id: 'w1',
-			name: 'Weronika',
-			image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
-			birds: 14,
-		},
-		{
-			id: 'w1',
-			name: 'Weronika',
-			image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
-			birds: 14,
-		},
-		{
-			id: 'w1',
-			name: 'Weronika',
-			image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
-			birds: 14,
-		},
-		{
-			id: 'w1',
-			name: 'Weronika',
-			image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
-			birds: 14,
-		},
-		{
-			id: 'w1',
-			name: 'Weronika',
-			image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
-			birds: 14,
-		},
+		// {
+		// 	id: 'w1',
+		// 	name: 'Weronika',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// 	birds: 14,
+		// },
+		// {
+		// 	id: 'w1',
+		// 	name: 'Weronika',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// 	birds: 14,
+		// },
+		// {
+		// 	id: 'w1',
+		// 	name: 'Weronika',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// 	birds: 14,
+		// },
+		// {
+		// 	id: 'w1',
+		// 	name: 'Weronika',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// 	birds: 14,
+		// },
+		// {
+		// 	id: 'w1',
+		// 	name: 'Weronika',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// 	birds: 14,
+		// },
+		// {
+		// 	id: 'w1',
+		// 	name: 'Weronika',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// 	birds: 14,
+		// },
 	];
 	return <UsersList items={USERS} />;
 };
