0e7824a296eecae1d7a662c877d3fb6704128bb6
Weronika Dziua <wer.dziura@gmail.com>
2023-12-05 22:16:20 +0100

    feat: create a portal for slider-hook

--- src/shared/components/Navigation/Slider.js
+++ src/shared/components/Navigation/Slider.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react';
+import { createPortal } from 'react-dom';
+
 import './Slider.scss';
 
 const Slider = props => {
-	return <aside className='slider'> {props.children}</aside>;
+	const element = <aside className='slider'> {props.children}</aside>;
+
+	return createPortal(element, document.getElementById('slider-hook'));
 };
 
 export default Slider;
