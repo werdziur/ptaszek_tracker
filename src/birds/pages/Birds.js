cc9946695c61de4e2060ec1c90d887b9c03e68c3
Weronika Dziua <wer.dziura@gmail.com>
2024-01-04 19:36:39 +0100

    feat: add list of birds as well as info when no birds found

---
+++ src/birds/pages/Birds.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import BirdsList from '../components/BirdsList';
+
+const Birds = props => {
+	const BIRDS = [
+		// {
+		// 	id: 'w1',
+		// 	name: 'Kos',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// },
+		// {
+		// 	id: 'w1',
+		// 	name: 'Ptak',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// },
+		// {
+		// 	id: 'w1',
+		// 	name: 'Kukułka',
+		// 	image: 'https://cdn.pixabay.com/photo/2020/05/27/16/56/robin-5228041_960_720.jpg',
+		// },
+	];
+	return <BirdsList items={BIRDS} />;
+};
+
+export default Birds;
