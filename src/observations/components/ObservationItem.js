6d6910e9ee231b0401092acee1c716ce909e8064
Weronika Dziua <wer.dziura@gmail.com>
2024-01-12 23:00:35 +0100

    feat: add user observation list and item js and css

---
+++ src/observations/components/ObservationItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from '../../shared/components/UIElements/Card';
+
+import './ObservationItem.scss';
+import ObservationsList from './ObservationsList';
+
+const ObservationItem = props => {
+	return (
+		<Card className='observation-item'>
+			{/* <Link to={`/${props.id}/places`}> */}
+			<div className='observation-item__image'>
+				<img className='observation-item__image--img' src={props.image} alt={props.name} />
+			</div>
+
+			<div className='observation-item__text'>
+				<div className='observation-item__name'>
+					<h2 className='observation-item__name--text'>{props.name}</h2>
+				</div>
+			</div>
+			{/* </Link> */}
+		</Card>
+	);
+};
+
+export default ObservationItem;
---
+++ src/observations/components/ObservationsList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Card from '../../shared/components/UIElements/Card';
+import './ObservationsList.scss';
+import ObservationItem from './ObservationItem';
+
+const ObservationsList = props => {
+	if (props.items.length === 0) {
+		return (
+			<div className='observation-list center'>
+				<Card className='observation-item'>
+					<h2>No birds found. Maybe create one?</h2>
+				</Card>
+			</div>
+		);
+	}
+	return (
+		<div className='observation-list'>
+			{props.items.map(observ => (
+				<ObservationItem key={observ.id} id={observ.id} image={observ.image} name={observ.name} user={observ.user} />
+			))}
+		</div>
+	);
+};
+
+export default ObservationsList;
