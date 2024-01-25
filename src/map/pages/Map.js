bf089d20aef4a6a574ee01730aaa813e7a2825b2
Weronika Dziua <wer.dziura@gmail.com>
2024-01-25 22:02:21 +0100

    feat: add function that takes user current position in latitude and longitude

---
+++ src/map/pages/Map.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import ShowMap from '../components/loadMap';
+
+const Map = () => {
+	const [currLocation, setCurrLocation] = useState({});
+
+	useEffect(() => {
+		getLocation();
+	}, []);
+
+	const getLocation = function () {
+		if (navigator.geolocation)
+			navigator.geolocation.getCurrentPosition(
+				position => setCurrLocation(position.coords),
+				function () {
+					alert('Could not get your position. Try again!');
+				}
+			);
+	};
+
+	return <ShowMap latitude={currLocation.latitude} longitude={currLocation.longitude} />;
+};
+export default Map;
